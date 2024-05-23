925107d7e26f08215c013f4604ebe78ed1b5d421
vancomm <m1901199@gmail.com>
2024-05-24 00:01:34 +0300

    WIP

--- main.go
+++ main.go
@@ -4,59 +4,32 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strings"
 
 	"github.com/gorilla/schema"
 )
 
-var requiredParams = []string{"w", "h", "b"}
-
-type FieldParams struct {
-	height int
-	width  int
-	bombs  int
+type QueryParams struct {
+	Height int `schema:"height"`
+	Width  int `schema:"width"`
+	Bombs  int `schema:"bombs"`
 }
 
 var decoder = schema.NewDecoder()
 
-func parseFieldParams(r *http.Request, paramName string) (int, error) {
-	var (
-		param int
-		err   error
-	)
-
-	rawParam := r.URL.Query().Get(paramName)
-	if rawParam == "" {
-		err = fmt.Errorf("missing param %s", paramName)
-		return param, err
-	}
-
-	if _, err := fmt.Sscan(rawParam, &param); err != nil {
-		err = fmt.Errorf("invalid param %s: must be integer", paramName)
-		return param, err
-	}
-
-	return param, err
-}
-
 func handleGetField(w http.ResponseWriter, r *http.Request) {
-	var buf strings.Builder
-	buf.WriteString("your search params: ")
-
-	for _, paramName := range requiredParams {
-		param, err := parseFieldParams(r, paramName)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprintln(w, err)
-		}
-		fmt.Fprintf(&buf, "%s: %d", paramName, param)
+	var query QueryParams
+	if err := decoder.Decode(&query, r.URL.Query()); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, err)
+		return
 	}
-
-	response := strings.TrimSuffix(buf.String(), ", ")
-	fmt.Fprintln(w, response)
+	response := fmt.Sprintf("query: %v\n", query)
+	log.Print(response)
+	fmt.Fprint(w, response)
 }
 
 func main() {
+
 	router := http.NewServeMux()
 
 	router.HandleFunc("GET /field", handleGetField)
